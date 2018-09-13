4a2252957ebf2a18b921c20ccc0aee92baa9802f
yznt <nsso@foxmail.com>
2018-09-13 10:17:28 +0800

    feed

--- packages/frontend/src/Connection.ts
+++ packages/frontend/src/Connection.ts
@@ -13,7 +13,7 @@ export class Connection {
 
   private static readonly address = window.location.protocol === 'https:'
                                       ? `wss://${window.location.hostname}/feed/`
-                                      : `ws://${window.location.hostname}:8080`;
+                                      : `ws://${window.location.hostname}/feed/`;
 
   // private static readonly address = 'wss://telemetry.polkadot.io/feed/';
 
